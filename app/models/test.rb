b7ca3642239e12ce1daabd269188d7e98085b11b
kerni2 <kerni2@ya.ru>
2022-07-15 18:30:52 +0300

    feat: add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :progresses
+  has_many :users, through: :progresses
+  belongs_to :author, class_name: :User, foreign_key: :author_id
   def self.category_name(name)
     Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', name).order(title: 'DESC').pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :progresses
+  has_many :tests, through: :progresses
+  has_many :authors, class_name: :Test, inverse_of: :author, foreign_key: :author_id
   def difficulty(level)
     Test.joins('JOIN progresses ON tests.id = progresses.test_id').where('progresses.user_id = ? AND tests.level = ?', self.id, level)
   end
