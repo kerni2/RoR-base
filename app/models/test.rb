4f6d87047d31c7fc446621dcd98a7cb701465eee
kerni2 <kerni2@ya.ru>
2022-07-18 17:58:23 +0300

    feat: add scope and validates

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,22 @@ class Test < ApplicationRecord
   has_many :users, through: :progresses
   belongs_to :author, class_name: :User, foreign_key: :author_id
 
-  def self.category_name(name)
-    Test.joins(:category).where(category: { title: name }).order(title: 'DESC').pluck('title')
+  scope :easy_test, -> { where(level: 0..1) }
+  scope :middle_test, -> { where(level: 2..4) }
+  scope :hard_test, -> { where(level: 5..Float::INFINITY) }
+
+  scope :category_name, -> (name) { joins(:category).where(category: { title: name }).order(title: 'DESC') }
+
+  def self.category_sort(name)
+    Test.category_name(name).pluck('title')
   end
+
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level }
+
+#  def self.category_name(name)
+#    Test.joins(:category).where(category: { title: name }).order(title: 'DESC').pluck('title')
+#  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   has_many :tests, through: :progresses
   has_many :author_tests, class_name: :Test, inverse_of: :author, foreign_key: :author_id
 
+
   def difficulty(level)
     tests.where(level: level)
   end
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :type_user, presence: true
+
 end
