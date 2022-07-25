483f472b703c0bd94144b839adb7d10aef0a9ae6
Anton Spiridonov <kerni2@ya.ru>
2022-07-25 18:31:52 +0300

    feat: add tests actions

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,4 +4,43 @@ class TestsController < ApplicationController
     @tests = Test.all
   end
 
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
 end
