1ce8bebb22ecd37eafd423006ab1d989dc7b7883
Anton Spiridonov <kerni2@ya.ru>
2022-09-15 16:47:47 +0300

    feat: add GitHub Gist

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: :Test, inverse_of: :author, foreign_key: :author_id
+  has_many :gists, dependent: :destroy
 
  # EMAIL_FORMAT = /[A-Z0-9._%+-]+@([A-Z0-9][-A-z0-9]+\.)+[A-z]{2,4}/i
 
