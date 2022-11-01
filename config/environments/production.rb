a7f4132ec511d3a1243bc4e2d539755e494ce39a
Anton Spiridonov <kerni2@ya.ru>
2022-11-01 17:44:51 +0300

    feat: edit host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'ror.mailer.1@gmail.com',
+    user_name:            'ror.mailer.1',
     password:             'aK2PR4nkTMAGCdx',
     authentication:       'plain',
     enable_starttls_auto: true }
