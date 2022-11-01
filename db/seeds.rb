136accb2c05aea401c676d2f0629586ca015ec5b
Anton Spiridonov <kerni2@ya.ru>
2022-11-01 07:52:31 +0300

    feat: change db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,55 +6,54 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(name: 'user1', email: 'user1@mail.com', group: 'user')
-user2 = User.create(name: 'user2', email: 'user2@mail.com', group: 'user')
-user3 = User.create(name: 'user3', email: 'user3@mail.com', group: 'user')
+user1 = User.create(email: 'user1@mail.com', password: '111111')
+user2 = User.create(email: 'user2@mail.com', password: '222222')
+user3 = Admin.create(email: 'user3@mail.com', password: '333333')
 
 python = Category.create(title: 'Python')
 ruby = Category.create(title: 'Ruby')
 web = Category.create(title: 'Web')
 
-test_ruby = Test.create(title: 'Ruby', level: 1, category_id: ruby.id, author_id: user1.id)
-test_web = Test.create(title: 'Web', category_id: web.id, author_id: user1.id)
-test_python = Test.create(title: 'Python', level: 1, category_id: python.id, author_id: user1.id)
+test_ruby = ruby.tests.create(title: 'Ruby', level: 1, author: user1)
+test_web = web.tests.create(title: 'Web', author: user1)
+test_python = python.tests.create(title: 'Python', level: 1, author: user1)
 
-ruby_question1 = Question.create(body: 'В каком варианте вы получите число без пропуска строки от пользователя:', test_id: test_ruby.id)
+ruby_question1 = test_ruby.questions.create!(body: 'В каком варианте вы получите число без пропуска строки от пользователя:', test_id: test_ruby.id)
 
-ruby_question1_answer1 = Answer.create(body: 'num = gets. to_i', question_id: ruby_question1.id)
-ruby_question1_answer2 = Answer.create(body: 'num = gets. chomp()', question_id: ruby_question1.id)
-ruby_question1_answer3 = Answer.create(body: 'num = gets. chomp(). to_i', correct: true, question_id: ruby_question1.id)
+ruby_question1_answer1 = ruby_question1.answers.create(body: 'num = gets. to_i', question_id: ruby_question1.id)
+ruby_question1_answer2 = ruby_question1.answers.create(body: 'num = gets. chomp()', question_id: ruby_question1.id)
+ruby_question1_answer3 = ruby_question1.answers.create(body: 'num = gets. chomp(). to_i', correct: true, question_id: ruby_question1.id)
 
-ruby_question2 = Question.create(body: 'Как называется самый популярный фреймворк Ruby для веба:', test_id: test_ruby.id)
+ruby_question2 = test_ruby.questions.create!(body: 'Как называется самый популярный фреймворк Ruby для веба:', test_id: test_ruby.id)
 
-ruby_question2_answer1 = Answer.create(body: 'Ruby Web', question_id: ruby_question2.id)
-ruby_question2_answer2 = Answer.create(body: 'Ruby Framework', question_id: ruby_question2.id)
-ruby_question2_answer3 = Answer.create(body: 'Ruby On Rails', correct: true, question_id: ruby_question2.id)
+ruby_question2_answer1 = ruby_question2.answers.create(body: 'Ruby Web', question_id: ruby_question2.id)
+ruby_question2_answer2 = ruby_question2.answers.create(body: 'Ruby Framework', question_id: ruby_question2.id)
+ruby_question2_answer3 = ruby_question2.answers.create(body: 'Ruby On Rails', correct: true, question_id: ruby_question2.id)
 
-web_question1 = Question.create(body: 'Каким тегом задается метка на web-странице?', test_id: test_web.id)
+web_question1 = test_web.questions.create!(body: 'Каким тегом задается метка на web-странице?', test_id: test_web.id)
 
-web_question1_answer1 = Answer.create(body: '<font color="..."> </font>', question_id: web_question1.id)
-web_question1_answer2 = Answer.create(body: '<a href="..."> </a>', question_id: web_question1.id)
-web_question1_answer3 = Answer.create(body: '<a name="..."></a>', correct: true, question_id: web_question1.id)
+web_question1_answer1 = web_question1.answers.create(body: '<font color="..."> </font>', question_id: web_question1.id)
+web_question1_answer2 = web_question1.answers.create(body: '<a href="..."> </a>', question_id: web_question1.id)
+web_question1_answer3 = web_question1.answers.create(body: '<a name="..."></a>', correct: true, question_id: web_question1.id)
 
-web_question2 = Question.create(body: 'Каким тегом объявляется web-страница?', test_id: test_web.id)
+web_question2 = test_web.questions.create!(body: 'Каким тегом объявляется web-страница?', test_id: test_web.id)
 
-web_question2_answer1 = Answer.create(body: '<head> </head>', question_id: web_question2.id)
-web_question2_answer2 = Answer.create(body: '<body> </body>', question_id: web_question2.id)
-web_question2_answer3 = Answer.create(body: '<html> </html>', correct: true, question_id: web_question2.id)
+web_question2_answer1 = web_question2.answers.create(body: '<head> </head>', question_id: web_question2.id)
+web_question2_answer2 = web_question2.answers.create(body: '<body> </body>', question_id: web_question2.id)
+web_question2_answer3 = web_question2.answers.create(body: '<html> </html>', correct: true, question_id: web_question2.id)
 
-python_question1 = Question.create(body: 'Какая из следующих функций преобразует одиночный символ в его целочисленный эквивалент в Python?', test_id: test_python.id)
+python_question1 = test_python.questions.create!(body: 'Какая из следующих функций преобразует одиночный символ в его целочисленный эквивалент в Python?', test_id: test_python.id)
 
-python_question1_answer1 = Answer.create(body: 'unichr(x)', question_id: python_question1.id)
-python_question1_answer2 = Answer.create(body: 'oct(x)', question_id: python_question1.id)
-python_question1_answer3 = Answer.create(body: 'ord(x)', correct: true, question_id: python_question1.id)
+python_question1_answer1 = python_question1.answers.create(body: 'unichr(x)', question_id: python_question1.id)
+python_question1_answer2 = python_question1.answers.create(body: 'oct(x)', question_id: python_question1.id)
+python_question1_answer3 = python_question1.answers.create(body: 'ord(x)', correct: true, question_id: python_question1.id)
 
-python_question2 = Question.create(body: 'Какая из следующих функций используется для проверки, все ли символы в строке находятся в нижнем регистре?', test_id: test_python.id)
+python_question2 = test_python.questions.create!(body: 'Какая из следующих функций используется для проверки, все ли символы в строке находятся в нижнем регистре?', test_id: test_python.id)
 
-python_question2_answer1 = Answer.create(body: 'isspace()', question_id: python_question2.id)
-python_question2_answer2 = Answer.create(body: 'istitle()', question_id: python_question2.id)
-python_question2_answer3 = Answer.create(body: 'islower()', correct: true, question_id: python_question2.id)
+python_question2_answer1 = python_question2.answers.create(body: 'isspace()', question_id: python_question2.id)
+python_question2_answer2 = python_question2.answers.create(body: 'istitle()', question_id: python_question2.id)
+python_question2_answer3 = python_question2.answers.create(body: 'islower()', correct: true, question_id: python_question2.id)
 
-TestPassage.create(user_id: user1.id, test_id: test_ruby.id)
-TestPassage.create(user_id: user1.id, test_id: test_web.id)
-TestPassage.create(user_id: user2.id, test_id: test_web.id)
-TestPassage.create(user_id: user3.id, test_id: test_python.id)
+test_ruby.users.push(user1)
+test_web.users.push(user1)
+test_python.users.push(user1)
